fba4da6b9434ca82137b94674fb53e1f7e72578a
Triet Phan <trietph@ahamove.com>
2019-10-25 16:49:30 +0700

    Updates

---
+++ precache-manifest.e0eb12bd2420db465b5e2a594abda8e9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac18ee9ac9162a4acd92f3bd35be5fe3",
+    "url": "/trietphan94/thefogteam1994.github.io/index.html"
+  },
+  {
+    "revision": "a9999a9edfc98adeafd7",
+    "url": "/trietphan94/thefogteam1994.github.io/static/css/main.f688d532.chunk.css"
+  },
+  {
+    "revision": "f55583b15fbc9a708815",
+    "url": "/trietphan94/thefogteam1994.github.io/static/js/2.18b58bcf.chunk.js"
+  },
+  {
+    "revision": "a9999a9edfc98adeafd7",
+    "url": "/trietphan94/thefogteam1994.github.io/static/js/main.3693fd2c.chunk.js"
+  },
+  {
+    "revision": "63bb1c9f4763420dc195",
+    "url": "/trietphan94/thefogteam1994.github.io/static/js/runtime~main.c374c294.js"
+  },
+  {
+    "revision": "f00e90b017eab0b1b39cf874732996f8",
+    "url": "/trietphan94/thefogteam1994.github.io/static/media/pacman.f00e90b0.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b89d8a42c22bac81673bfbe7a732f682.js"
+  "/trietphan94/thefogteam1994.github.io/precache-manifest.e0eb12bd2420db465b5e2a594abda8e9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/trietphan94/thefogteam1994.github.io/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
