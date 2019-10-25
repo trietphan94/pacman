28dbe004e6b6fdf7cffbeac7e558b64a85c4361b
Triet Phan <trietph@ahamove.com>
2019-10-25 16:40:23 +0700

    Updates

---
+++ precache-manifest.2fabbe3e65138bd9af77343ba0a06e79.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4b2aec077bbe6453c9648588a61d1ce",
+    "url": "pacmantest.github.io/index.html"
+  },
+  {
+    "revision": "f10617e515a19f03eb82",
+    "url": "pacmantest.github.io/static/css/main.f688d532.chunk.css"
+  },
+  {
+    "revision": "f55583b15fbc9a708815",
+    "url": "pacmantest.github.io/static/js/2.18b58bcf.chunk.js"
+  },
+  {
+    "revision": "f10617e515a19f03eb82",
+    "url": "pacmantest.github.io/static/js/main.e707f9df.chunk.js"
+  },
+  {
+    "revision": "add66ee762fe776c2ebb",
+    "url": "pacmantest.github.io/static/js/runtime~main.e70689cc.js"
+  },
+  {
+    "revision": "f00e90b017eab0b1b39cf874732996f8",
+    "url": "pacmantest.github.io/static/media/pacman.f00e90b0.png"
+  }
+]);
